8188487ea60a7c3c3a2ba9a6d495a147d113a5be
7-Zip <mattkorn129@gmail.com>
2019-06-02 15:33:42 -0400

    Updates

---
+++ precache-manifest.cd348040c5edd9bc46beaff3f7c50d20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70f67d8050a3c1b58ae00cfb10ad926",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "0b8b5a0f14768afc4c53",
+    "url": "/React-Todo-List/static/css/main.ed310113.chunk.css"
+  },
+  {
+    "revision": "73b3b7de436b87a9df8b",
+    "url": "/React-Todo-List/static/js/2.86706d93.chunk.js"
+  },
+  {
+    "revision": "0b8b5a0f14768afc4c53",
+    "url": "/React-Todo-List/static/js/main.f9c09525.chunk.js"
+  },
+  {
+    "revision": "a8a38d11c11fa70ea7a1",
+    "url": "/React-Todo-List/static/js/runtime~main.34b6e918.js"
+  }
+]);
